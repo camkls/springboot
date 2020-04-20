407974c7cc80b8f71738259106fce99f99497674
camkls <cam_kls@163.com>
2020-04-20 15:27:45 +0800

    Initial commit

---
+++ src/main/java/cn/sz/lb/Bootapplication.java
@@ -0,0 +1,11 @@
+package cn.sz.lb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class,args);
+    }
+}
